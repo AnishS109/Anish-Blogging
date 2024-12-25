b153da014683684be57203cbbc575d70efd887c7
AnishS109 <anishsaini9098@gmail.com>
2024-12-25 16:53:29 +0530

    and

--- Backend/database/database.js
+++ Backend/database/database.js
@@ -4,6 +4,8 @@ const ConnectionDB = async() => {
 
   const URL = "mongodb+srv://root:6TmgY4T39vnnMS6b@lms.8dvli.mongodb.net/BLOG?retryWrites=true&w=majority";
 
+
+
   try {
 
     await mongoose.connect(URL)
